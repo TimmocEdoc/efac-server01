b882f52b7207397f495c9a1405e5135fccba0def
TimmocEdoc <fraggler131@gmail.com>
2020-10-15 14:39:04 +0700

    fix

--- src/main/java/com/cafe/configuration/AppConfig.java
+++ src/main/java/com/cafe/configuration/AppConfig.java
@@ -1,8 +1,14 @@
 package com.cafe.configuration;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class AppConfig extends WebMvcConfigurerAdapter {
@@ -12,5 +18,15 @@ public class AppConfig extends WebMvcConfigurerAdapter {
                 .addResourceLocations("classpath:/META-INF/resources/");
         registry.addResourceHandler("/webjars/**")
                 .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+    @Value("${spring.datasource.url}")
+    private String dbUrl;
+
+    @Bean
+    public DataSource dataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(dbUrl);
+        return new HikariDataSource(config);
     }
 }
